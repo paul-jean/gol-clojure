2c669a4d25f82e0fb238203e1ba6a88aa562ecbb
Sid Raval <sidsraval@gmail.com>
2014-04-20 16:05:06 -0400

    Update function working. Separates business logic functions into new file.

---
+++ game-of-life.clj
@@ -0,0 +1,26 @@
+(defn sum-excluding-middle [matrix]
+  (- (reduce + (flatten matrix)) (get-in matrix [1 1])))
+
+(defn neighborhood [matrix row col]
+  (vec (map #(subvec % (- col 1) (+ col 2)) (subvec matrix (- row 1) (+ row 2)))))
+
+(defn new-val [nbhd]
+  (let [s (sum-excluding-middle nbhd) center ((nbhd 1) 1)]
+    (if (= center 0)
+      (if (= s 3) 1 0)
+      (if (< s 2) 0
+        (if (or (= s 2) (= s 3)) 1 0)))))
+
+(defn pad-matrix [matrix]
+  (let [cols (+ (count (matrix 0)) 2)]
+    (into (into [(vec (replicate cols 0))] (map #(into (into [0] %) [0]) matrix)) [(vec (replicate cols 0))])
+  ))
+
+(defn new-row [matrix row]
+  (let [cols (count (matrix 0))]
+    (map #(new-val (neighborhood matrix row %))
+         (range 1 (- cols 1)))))
+
+(defn update [matrix]
+    (let [rows (count matrix) cols (count (matrix 0))]
+      (pad-matrix (mapv #(vec (new-row matrix %)) (range 1 (- rows 1))))))
